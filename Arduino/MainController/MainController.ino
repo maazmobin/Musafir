ae0c95617839102f4c91c9906ba0f808ec84c5f5
Zaid Pirwani <email@zaidpirwani.com>
2016-10-24 12:46:11 +0500

    PC to MainController to MotorController Tested
    RADIO Works, Payload MAX Size is 32bits, rest is TRUNCATED.

--- Arduino/MainController/MainController.ino
+++ Arduino/MainController/MainController.ino
@@ -6,84 +6,90 @@
 
 // RF24 radio(CE,CSN);
 RF24 radio(49,53);
-const uint64_t pipes[2] = { 0xDEDEDEDEE8LL, 0xDEDEDEDEE4LL};
+const uint64_t pipes[2] = {0xDEDEDEDEE8LL, 0xDEDEDEDEE4LL};
 
 boolean stringComplete = false;
-static int dataBufferIndex = 0;
-boolean stringOverflow = false;
-char charOverflow = 0;
-String SendPayload = "";
-char RecvPayload[31] = "";
-char serialBuffer[31] = "";
+String sendPayload = "";
+String recvPayload = "";
 
 void setup(void) {
   Serial.begin(115200);
-  //Serial1.begin(115200);
+  Serial1.begin(115200);
   printf_begin();
+
+  Serial.println("MainController");
+  radioInit();
+  Serial.println();
+  sendPayload.reserve(32);
+  recvPayload.reserve(32);
+}
+
+void loop(void) {
+  //readIMU();
+  //readDMP();
+  radioReceive();  // radio to serial/process
+  serialReceive(); // serial to radio (AFTER PROCESS)
+}
+
+
+void radioInit(void){
   radio.begin();
+  radio.setAutoAck(1);
+  radio.enableAckPayload();
   radio.setDataRate(RF24_1MBPS);
   radio.setPALevel(RF24_PA_MAX);
   radio.setChannel(70);
   radio.enableDynamicPayloads();
-  radio.setRetries(0,15);
+  radio.setRetries(5,5);
   radio.setCRCLength(RF24_CRC_16);
   radio.openWritingPipe(pipes[0]);
   radio.openReadingPipe(1,pipes[1]);
   radio.startListening();
   radio.printDetails();
-  Serial.println();
-  Serial.println("RF Chat V01.0");
-  delay(500);
-}
-void loop(void) {
-  nRF_receive();
-  serial_receive();
 }
 
 void serialEvent1() {
   while (Serial1.available()) {
     char inChar = (char)Serial1.read();
-    SendPayload += inChar;
+    sendPayload += inChar;
     if (inChar == '\n') {
       stringComplete = true;
     }
   }
 }
 
-void nRF_receive(void) {
+void radioReceive(void) {
   int len = 0;
   if ( radio.available() ) {
     len = radio.getDynamicPayloadSize();
-    radio.read(&RecvPayload,len);
-    RecvPayload[len] = 0;
-    Serial.print(RecvPayload);
+    char buff[32]="";
+    radio.read(buff,len);
+    buff[len] = 0;
+    buff[31] = 0;
+    recvPayload += buff;
+    Serial.print(recvPayload);
     Serial.println();
-    String a=String(RecvPayload);
-    if (a.startsWith("MOTOR")) {
-      a= a.substring(6);
-      Serial1.println(a);
+    if (recvPayload.startsWith("MOTOR")) {
+      recvPayload=recvPayload.substring(6);
+      Serial1.print(recvPayload);
     }
-    RecvPayload[0] = 0;
+    recvPayload = "";
   }
 }
 
-void serial_receive(void) {
+void serialReceive(void) {
   if (stringComplete) {
+    // swap TX & Rx addr for writing
     radio.openWritingPipe(pipes[1]);
     radio.openReadingPipe(0,pipes[0]);
     radio.stopListening();
-    char c[31]="";
-    SendPayload.toCharArray(c,SendPayload.length());
-    delay(2);
-    //Delay is necessary, else transmission stops
-    radio.write(c,sizeof(c));
-    Serial.println(c);
-    stringComplete = false;
-    
+    char buff[32]="";
+    sendPayload.toCharArray(buff,32);
+    radio.write(buff,strlen(buff));
     radio.openWritingPipe(pipes[0]);
     radio.openReadingPipe(1,pipes[1]);
     radio.startListening();
-    SendPayload="";
-    dataBufferIndex = 0;
+    sendPayload = "";
+    stringComplete = false;
   }
 }
--- Arduino/PcNrfRelay/PcNrfRelay.ino
+++ Arduino/PcNrfRelay/PcNrfRelay.ino
@@ -6,121 +6,81 @@
 
 // RF24 radio(CE,CSN);
 RF24 radio(49,53);
+const uint64_t pipes[2] = {0xDEDEDEDEE8LL, 0xDEDEDEDEE4LL};
 
-const uint64_t pipes[2] = { 0xDEDEDEDEE8LL, 0xDEDEDEDEE4LL };
-
-boolean stringComplete = false;  // whether the string is complete
-static int dataBufferIndex = 0;
-boolean stringOverflow = false;
-char charOverflow = 0;
-
-char SendPayload[31] = "";
-char RecvPayload[31] = "";
-char serialBuffer[31] = "";
+boolean stringComplete = false;
+String sendPayload = "";
+String recvPayload = "";
 
 void setup(void) {
- 
   Serial.begin(115200);
-  
   printf_begin();
-  radio.begin();
-    radio.setAutoAck(1);                    // Ensure autoACK is enabled
-  radio.enableAckPayload();               // Allow optional ack payloads
 
-  radio.setDataRate(RF24_1MBPS);
-  //radio.setDataRate(RF24_1MBPS);
-  radio.setPALevel(RF24_PA_MAX);
-  radio.setChannel(70);
-  
-  radio.enableDynamicPayloads();
-  radio.setRetries(0,15);
-  radio.setCRCLength(RF24_CRC_16);
-
-  radio.openWritingPipe(pipes[0]);
-  radio.openReadingPipe(1,pipes[1]);  
-  
-  radio.startListening();
-  radio.printDetails();
-
+  Serial.println("PcNrfRelay");
+  radioInit();
   Serial.println();
-  Serial.println("RF Chat V01.0");    
-  delay(500);
+  sendPayload.reserve(32);
+  recvPayload.reserve(32);
 }
 
 void loop(void) {
-  
-  nRF_receive();
-  serial_receive();
-  
-} // end loop()
+  radioReceive();  // radio to serial
+  serialReceive(); // serial to radio (AFTER PROCESS)
+}
+
+void radioInit(void){
+  radio.begin();
+  radio.setAutoAck(1);
+  radio.enableAckPayload();
+  radio.setDataRate(RF24_1MBPS);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setChannel(70);
+  radio.enableDynamicPayloads();
+  radio.setRetries(5,5);
+  radio.setCRCLength(RF24_CRC_16);
+  radio.openWritingPipe(pipes[0]);
+  radio.openReadingPipe(1,pipes[1]);
+  radio.startListening();
+  radio.printDetails();
+}
 
 void serialEvent() {
-  while (Serial.available() > 0 ) {
-      char incomingByte = Serial.read();
-      
-      if (stringOverflow) {
-         serialBuffer[dataBufferIndex++] = charOverflow;  // Place saved overflow byte into buffer
-         serialBuffer[dataBufferIndex++] = incomingByte;  // saved next byte into next buffer
-         stringOverflow = false;                          // turn overflow flag off
-      } else if (dataBufferIndex > 31) {
-         stringComplete = true;        // Send this buffer out to radio
-         stringOverflow = true;        // trigger the overflow flag
-         charOverflow = incomingByte;  // Saved the overflow byte for next loop
-         dataBufferIndex = 0;          // reset the bufferindex
-         break; 
-      } 
-      else if(incomingByte=='\n'){
-          serialBuffer[dataBufferIndex] = 0; 
-          stringComplete = true;
-      } else {
-          serialBuffer[dataBufferIndex++] = incomingByte;
-          serialBuffer[dataBufferIndex] = 0; 
-      }          
-  } // end while()
-} // end serialEvent()
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    sendPayload += inChar;
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
+}
 
-void nRF_receive(void) {
+void radioReceive(void) {
   int len = 0;
   if ( radio.available() ) {
-        len = radio.getDynamicPayloadSize();
-        radio.read(&RecvPayload,len);
-        //delay(5);
-  
-    RecvPayload[len] = 0; // null terminate string
-    
- //   Serial.print("R:");
-    Serial.print(RecvPayload);
+    len = radio.getDynamicPayloadSize();
+    char buff[32]="";
+    radio.read(buff,len);
+    buff[len] = 0;
+    buff[31] = 0;
+    recvPayload += buff;
+    Serial.print(recvPayload);
     Serial.println();
-    RecvPayload[0] = 0;  // Clear the buffers
-  }  
-
-} // end nRF_receive()
-
-void serial_receive(void){
-  
-  if (stringComplete) { 
-        strcat(SendPayload,serialBuffer);      
-        // swap TX & Rx addr for writing
-        radio.openWritingPipe(pipes[1]);
-        radio.openReadingPipe(0,pipes[0]);  
-        radio.stopListening();
-        bool a = radio.write(&SendPayload,strlen(SendPayload));
-        if(a)
-        {
-          Serial.println("Ssent");
-        }
-       // Serial.print("S:");  
-    //    Serial.print(SendPayload);          
-     //   Serial.println();
-        stringComplete = false;
-       // Serial.println();
-        // restore TX & Rx addr for reading  
-             
-        radio.openWritingPipe(pipes[0]);
-        radio.openReadingPipe(1,pipes[1]);
-         
-        radio.startListening();  
-        SendPayload[0] = 0;
-        dataBufferIndex = 0;
-  } // endif
-} // end serial_receive()
+    recvPayload = "";
+  }
+}
+void serialReceive(void) {
+  if (stringComplete) {
+    // swap TX & Rx addr for writing
+    radio.openWritingPipe(pipes[1]);
+    radio.openReadingPipe(0,pipes[0]);
+    radio.stopListening();
+    char buff[32]="";
+    sendPayload.toCharArray(buff,32);
+    radio.write(buff,strlen(buff));
+    radio.openWritingPipe(pipes[0]);
+    radio.openReadingPipe(1,pipes[1]);
+    radio.startListening();
+    sendPayload = "";
+    stringComplete = false;
+  }
+}
