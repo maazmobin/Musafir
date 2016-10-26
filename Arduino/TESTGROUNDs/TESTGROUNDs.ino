6b69e53bac56a53d0669d174e51e20321bee31e7
Zaid Pirwani <email@zaidpirwani.com>
2016-10-26 12:46:17 +0500

    added PID code - BUT
    PID code for bpoth motors added,
    BUT
    The Navigator gives only Speed of Robot, NOT of individual motors, NEED
    to read and implement PILOT as well, to utilize PID for motors.

--- Arduino/TESTGROUNDs/TESTGROUNDs.ino
+++ Arduino/TESTGROUNDs/TESTGROUNDs.ino
@@ -1,4 +1,5 @@
 #include <math.h>
+
 #include <Encoder.h>
 Encoder encL(2,4);
 Encoder encR(3,5);
@@ -8,25 +9,32 @@ long encCurrL, encCurrR;
 MusafirMotor motorL(7, 6, 9);
 MusafirMotor motorR(13, 12, 10);
 
-double spdL, spdR;
-#define WHEEL_DIAMETER_CM    9.15  // centimeter
-#define TICKS_PER_REV     1500
-#define DISTANCE_PER_TICK (float)((M_PI*WHEEL_DIAMETER_CM)/TICKS_PER_REV)
-
-int interval=100;
-
 #include "Navigator.h"
 Navigator  navigator;
 //from https://github.com/solderspot/NavBot/blob/master/NavBot_v1/BlankBot.h
 // Navigator defines
 #define WHEELBASE               nvMM(190)      // millimeters
 #define WHEEL_DIAMETER          nvMM(91.5)     // millimeters
-
+#define TICKS_PER_REV           1500
 // correct for systematic errors
 #define WHEEL_RL_SCALER         1.0f  // Ed
 #define WHEELBASE_SCALER        1.0f  // Eb
 // correct distance 
 #define DISTANCE_SCALER         1.0f  // Es
+
+#include <PID_v1.h>
+double measuredVelL=0, measuredVelR=0;
+double pwmL=0, pwmR=0;
+double velL=0, velR=0;
+// PID (&input, &output, &setpoint, kp, ki, kd, DIRECT/REVERSE)
+PID pidL(&measuredVelL, &pwmL, &velL, 2,1,0, DIRECT);
+PID pidR(&measuredVelR, &pwmR, &velR, 2,1,0, DIRECT);
+
+boolean pidActive= false;
+
+unsigned long previousMillis = 0;
+int interval = 10; // in ms
+int debugInterval = 1000; // in ms
 
 void setup() {
   Serial.begin(115200);
@@ -37,35 +45,46 @@ void setup() {
   navigator.SetDistanceScaler( DISTANCE_SCALER );
   navigator.SetWheelbaseScaler( WHEELBASE_SCALER );
   navigator.SetWheelRLScaler( WHEEL_RL_SCALER );
-  navigator.Reset(millis());  
+  navigator.SetMinInterval(interval);
+  navigator.Reset(millis());
+
+  initPID();
 }
 
-int speeds[] = {0, 20, 30, 40, 50, 80, 100, 125, 150, 175, 200, 225, 250, 254, 255,
-                254, 250, 225, 200, 175, 150, 125, 100, 80, 50, 40, 30, 20, 0};
+void loop() {   
+  pidL.Compute();
+  pidR.Compute();
+  if(velL>0) motorL.setPWM(pwmL);
+  else motorL.setPWM(0);
+  if(velR>0) motorR.setPWM(pwmR);
+  else motorR.setPWM(0);
 
-void loop() {
-    encCurrL = encL.read();
-    encCurrR =-encR.read();
-    navigator.UpdateTicks(encCurrL, encCurrR, millis());
-    encL.write(0); encR.write(0);
-    int currSecond = millis()/1000;
-    if(currSecond>0 && currSecond<30){
-      motorL.setPWM(50);
-      motorR.setPWM(50);
-      Serial.print(millis());
-      Serial.print(", ");
-      Serial.print(navigator.Position().x/10);
-      Serial.print(", ");
-      Serial.print(navigator.Position().y/10);
-      Serial.print(", ");
-      Serial.print(navigator.Heading());
-      Serial.print(", ");
-      Serial.print(navigator.TurnRate());
-      Serial.print(", ");
-      Serial.println(navigator.Speed()/10);
-    }else{
-      motorL.setPWM(0);
-      motorR.setPWM(0);
-    }
-    delay(interval);
+  encCurrL = encL.read(); encL.write(0); 
+  encCurrR =-encR.read(); encR.write(0);
+  navigator.UpdateTicks(encCurrL, encCurrR, millis());
+
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= debugInterval) {
+    previousMillis = currentMillis;
+    Serial.print(millis());
+    Serial.print(", ");
+    Serial.print(navigator.Position().x/10);
+    Serial.print(", ");
+    Serial.print(navigator.Position().y/10);
+    Serial.print(", ");
+    Serial.print(navigator.Heading());
+    Serial.print(", ");
+    Serial.print(navigator.TurnRate());
+    Serial.print(", ");
+    Serial.println(navigator.Speed()/10);
+  }
+}
+
+void initPID(void){
+  pidL.SetMode(MANUAL); // PID CONTROL OFF
+  pidR.SetMode(MANUAL);
+  pidL.SetSampleTime(interval); // sample time for PID
+  pidR.SetSampleTime(interval);
+  pidL.SetOutputLimits(0,250);  // min/max PWM
+  pidR.SetOutputLimits(0,250);  
 }
